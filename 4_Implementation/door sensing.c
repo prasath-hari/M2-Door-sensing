521524339dc4b06417cc806d1bfd159466515a4d
prasath-hari <101242023+prasath-hari@users.noreply.github.com>
2022-04-21 19:29:52 +0530

    added the c coding

---
+++ 4_Implementation/door sensing.c
@@ -0,0 +1,21 @@
+/*
+ * GccApplication6.c
+ *
+ * Created: 21-04-2022 10:31:16 AM
+ * Author : prasa
+ */ 
+
+#include <avr/io.h>
+int main(void)
+{
+	DDRB=DDRB&0b11111101;//fd
+	DDRC=DDRC|0b01000000;//40
+	while(1)
+	{
+		if(PINB & 0b00000010)//02
+		PORTC=PORTC|0b01000000;//40
+		else
+		PORTC=PORTC&0b10111111;//bf
+	}
+	return 0;
+}
